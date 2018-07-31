13a0a9a199248a2313dda828e3756a02c21b7264
Nikita Medvedev <some.redteapot@gmail.com>
2018-07-31 21:13:05 +0700

    Add break after first CMDEx match to command_upgrader_base

--- commands/upgrader/utils/command_upgrader_base.py
+++ commands/upgrader/utils/command_upgrader_base.py
@@ -1,12 +1,21 @@
+import logging
 from commands.pre_1_13.cmdex import CMDEx
+
+
+logger = logging.getLogger(__name__)
 
 def upgrade(CMDEXs: list, command: str, callback: callable) -> str:
     order = None
     props = None
 
+    logger.debug('Matching command \'%s\'', command)
+
     for cmdex in CMDEXs:
         try:
+            logger.debug('Matching CMDEx \'%s\'', str(cmdex))
             order, props = cmdex.match(command)
+            logger.debug('Match')
+            break
         except:
             pass
     
