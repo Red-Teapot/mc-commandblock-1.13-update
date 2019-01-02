4d9064c9cb1c89b422d6b85a3912fe2b88041882
Nikita Medvedev <some.redteapot@gmail.com>
2019-01-02 22:48:12 +0700

    Implement tellraw text selector upgrading

--- commands/upgrader/commands/tellraw.py
+++ commands/upgrader/commands/tellraw.py
@@ -1,6 +1,7 @@
 import json
 from ..utils import selector
 from commands.pre_1_13.cmdex import CMDEx
+from commands.upgrader.utils import json_text
 
 
 cmdex = CMDEx('tellraw {selector:selector} {json:message}')
@@ -10,4 +11,6 @@ def upgrade(command: str) -> str:
 
     new_selector = selector.upgrade(props['selector'])
 
-    return 'tellraw {} {}'.format(new_selector, json.dumps(props['message'], ensure_ascii=False))
+    new_message = json_text.upgrade(props['message'])
+
+    return 'tellraw {} {}'.format(new_selector, json.dumps(new_message, ensure_ascii=False))
---
+++ commands/upgrader/utils/json_text.py
@@ -0,0 +1,24 @@
+from . import selector
+from commands.pre_1_13.parser.tokenizer import Tokenizer
+
+
+def upgrade(json_text) -> dict:
+    if type(json_text) in [str, float, int, bool, None]:
+        return json_text
+    elif type(json_text) is list:
+        return [upgrade(x) for x in json_text]
+    elif type(json_text) is dict:
+        text = json_text.copy()
+
+        if 'selector' in text:
+            tokenizer = Tokenizer(text['selector'])
+            sel = tokenizer.expect_selector(pop=True)
+            text['selector'] = selector.upgrade(sel)
+        if 'score' in text and 'name' in text['score']:
+            tokenizer = Tokenizer(text['score']['name'])
+            sel = tokenizer.expect_selector(pop=True)
+            text['score']['name'] = selector.upgrade(sel)
+
+        return text
+    else:
+        raise Exception('Unknown JSON text type: {}'.format(type(json_text)))
